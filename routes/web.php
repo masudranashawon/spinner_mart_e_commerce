5c22c58a4622a15646863163899821ea74393d35
Masud Rana Shawon <mohammadmasudrana3396@gmail.com>
2025-11-29 01:49:07 +0600

    unwanted space removed

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-
-
-
 Route::controller(AdminController::class)->group(
     function () {
         Route::get("/", "index")->name("admin.root");
