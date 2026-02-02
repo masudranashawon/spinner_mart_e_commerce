b3a0d3bba81f7bce391ac8b40077d2f42ff7196b
Masud Rana Shawon <mohammadmasudrana3396@gmail.com>
2026-02-02 22:38:28 +0600

    login page added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('auth.layouts.app')
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
-</head>
+@section('title', 'Login')
 
-<body>
+@section('content')
+<div class="wpo-login-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <form class="wpo-accountWrapper" action="#">
+                    <div class="wpo-accountInfo">
+                        <div class="wpo-accountInfoHeader">
+                            <a href="{{ route('home') }}"><img src="{{ asset('frontend/assets/images/logo-2.svg') }}" alt=""></a>
+                            <a class="wpo-accountBtn" href="{{ route('register') }}">
+                                <span class="">Create Account</span>
+                            </a>
+                        </div>
+                        <div class="image">
+                            <img src="{{ asset('frontend/assets/images/login.svg') }}" alt="">
+                        </div>
+                        <div class="back-home">
+                            <a class="wpo-accountBtn" href="{{ route('home') }}">
+                                <span class="">Back To Home</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="wpo-accountForm form-style">
+                        <div class="fromTitle">
+                            <h2>Login</h2>
+                            <p>Sign into your pages account</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-12">
+                                <label>Email</label>
+                                <input type="text" id="email" name="email" placeholder="demo@gmail.com">
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-12">
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input class="pwd6" type="password" placeholder="" value="123456" name="pass">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-default reveal6" type="button"><i class="ti-eye"></i></button>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-12">
+                                <div class="check-box-wrap">
 
-</body>
-
-</html>
+                                    <div class="forget-btn">
+                                        <a href="forgot.html">Forgot Password?</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-12">
+                                <button type="submit" class="wpo-accountBtn">Login</button>
+                            </div>
+                        </div>
+                        <h4 class="or"><span>OR</span></h4>
+                        <ul class="wpo-socialLoginBtn">
+                            <li><button class="bg-danger" tabindex="0" type="button"><span><i class="ti-google"></i></span></button></li>
+                            <li>
+                                <button class="bg-secondary" tabindex="0" type="button"><span><i class="ti-github"></i></span></button>
+                            </li>
+                        </ul>
+                        <p class="subText">Don't have an account? <a href="{{ route('register') }}">Create free
+                                account</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
